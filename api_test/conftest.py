2944a152819d5f8971aec66ce71959e505553c96
zhangting <89507288@qq.com>
2019-08-05 21:13:15 +0800

    fix typo

--- api_test/conftest.py
+++ api_test/conftest.py
@@ -5,3 +5,7 @@ from Environment import Env
 @pytest.fixture(scope="module", autouse=True)
 def env():
     yield Env(token=os.environ['token'])
+
+@pytest.fixture(scope="module", autouse=True)
+def just_print():
+    print("我只是打印一段文本")
