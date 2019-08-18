ea3209fcee3f6b3970e904268ddf2da223f6f754
zhangting <89507288@qq.com>
2019-08-18 22:01:57 +0800

    add a path workaround

--- api_test/conftest.py
+++ api_test/conftest.py
@@ -1,4 +1,14 @@
-import pytest, os
+import pytest, os, sys
+
+parent=os.path.dirname
+libraries=parent(parent(__file__))+os.sep+"libraries"
+api=parent(parent(parent(__file__)))+os.sep+"TUGithubAPI"
+if libraries not in sys.path:
+    sys.path.append(libraries)
+if api not in sys.path:
+    sys.path.append(api)
+
+
 from Environment import Env
 from configparser import ConfigParser
 
@@ -8,3 +18,4 @@ def env():
     config.read('../data/data.ini')
     api_root_url=config[os.environ['env']]['api_root_url']
     yield Env(api_root_url=api_root_url,token=os.environ['token'])
+
